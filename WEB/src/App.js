153b3613957d2a4d35221f543ac572a7f2255038
Anderson Barros Silva <anderson.tec12@gmail.com>
2022-02-16 15:23:12 -0300

    Add web site project

---
+++ WEB/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import Header from './Header'
+import Route from './routes'
+
+import './global.css'
+function App() {
+  
+
+  // return (
+  //   <>
+  //     <Header>contdor: {counter}</Header>
+  //     <button onClick={  increment }>Adicionar</button>
+  //   </>
+  // );
+  return (
+    <>
+      <Route></Route>
+    </>
+  );
+}
+
+export default App;
